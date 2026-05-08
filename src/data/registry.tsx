d2c2a1e92355e39786292fdabf19762b3af3089c
BalaPriyan <balapriyanbalusamy@gmail.com>
2026-05-08 23:03:58 +0530

    feat: card added
    Fixes #4

--- src/data/registry.tsx
+++ src/data/registry.tsx
@@ -1,11 +1,13 @@
 import { CategoryDef } from "@/types";
 import { buttonDef } from "./generators/buttons";
+import { cardDef } from "./generators/card";
 
 export const REGISTRY: CategoryDef[] = [
   {
     category: "Elements",
     items: [
       buttonDef,
+      cardDef
     ],
   },
 ];
